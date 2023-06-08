670a6b4ae458c8aca1e5f96a0efba70b53489204
Snehal-23 <snehal.bodekar@outlook.com>
2023-06-08 18:51:53 +0530

    template added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+# import socket
+# socket.getaddrinfo('localhost', 8080)
 
 app = Flask(__name__)
 
@@ -7,4 +9,4 @@ def helloWorld():
     return render_template('home.html')
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1:5000',debug= True)
+    app.run(host='127.0.0.0',debug= True)
